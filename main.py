6e82737384392367f8974aed6897c611f0932624
ianku-ulendo <165811655+ianku-ulendo@users.noreply.github.com>
2025-02-11 11:10:51 -0500

    change config defaults key

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ terminal_output = []
 is_activated = False
 
 config_defaults = {
-    "Default": {
+    "default": {
         "data": DEFAULT_DATA_DIR,
         "output": DEFAULT_OUTPUT_DIR,
         "license_key": "",
